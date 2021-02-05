a62c97ab4f7b834a3fffa9e9f5f6ae17698f5af9
cemalyavuzg <66168236+cemalyavuzg@users.noreply.github.com>
2021-02-05 14:22:30 +0300

    Add files via upload

---
+++ variables/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sayı = 10;
+        String kelime = "öğrenci sayısı: ";
+        System.out.println("öğrencim: 11");
+        System.out.println("öğrencim: 11");
+        System.out.println("öğrencim: 11");
+        System.out.println("öğrencim: 11");
+        System.out.println(kelime + sayı);
+    }
+}
