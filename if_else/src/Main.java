4cf58a617b2ae8450e0d9eb3853e6d277410b17d
cemalyavuzg <66168236+cemalyavuzg@users.noreply.github.com>
2021-02-05 14:23:19 +0300

    Add files via upload

---
+++ if_else/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int sayı = 24;
+
+	if(sayı>20){
+	    System.out.println("sayı 20 den büyüktür");
+    }
+	else if(sayı == 20){
+	    System.out.println("sayı 20 ye eşittir");
+    }
+	if(sayı<20){
+	    System.out.println("sayı 20 den küçüktür");
+    }
+    }
+}
